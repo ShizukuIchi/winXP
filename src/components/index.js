2b846d462b827343d4f0019a033d49e1e24c5d0e
ShizukuIchi <shizukuichi@gmail.com>
2019-03-27 11:00:29 +0800

    clear

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,2 @@
-export { default as Options } from './Options';
-export { default as CloseButton } from './CloseButton';
 export { default as GoogleAnalytics } from './GoogleAnalytics';
-export { default as NoMatch } from './NoMatch';
-export { default as WindowDropdown } from './WindowDropdown'
+export { default as WindowDropdown } from './WindowDropdown';
