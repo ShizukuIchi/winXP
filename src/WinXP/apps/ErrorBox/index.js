94a3d0c981c0b2e9f4a641f3d2e1d834f0600572
ghostslayer989 <45863583+ghostslayer989@users.noreply.github.com>
2019-04-07 00:16:49 -0400

    Add files via upload

--- src/WinXP/apps/ErrorBox/index.js
+++ src/WinXP/apps/ErrorBox/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import error from 'src/assets/windowsIcons/897(32x32).png';
-import ReactAudioPlayer from 'react-audio-player';
-
-//Error sound
-<ReactAudioPlayer
-src="/winXP/src/assets/sounds/Windows XP Error.wav"
-autoPlay
-/>
+import Sound from 'react-sound';
+import beep from "../../../assets/sounds/Windows XP Error.wav";
 
 function Error({ onClose }) {
+  // //Error sound
+
   return (
     <Div>
+     <Sound
+      url={beep}
+      playStatus={Sound.status.PLAYING}
+      playFromPosition={0 /* in milliseconds */}
+      // onLoading={this.handleSongLoading}
+      // onPlaying={this.handleSongPlaying}
+      // onFinishedPlaying={this.handleSongFinishedPlaying}
+    />
       <div className="error__top">
         <img src={error} alt="error" className="error__img" />
         <div className="error__messages">
