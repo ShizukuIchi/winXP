94b292abb08f4e451cef312845db9031d7dd40d1
Tomer Matmon <tomer.matmon@reallygood.co.il>
2021-03-25 11:23:59 +0200

    Add reducer to display properties.

--- src/WinXP/apps/DisplayProperties/index.js
+++ src/WinXP/apps/DisplayProperties/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import TabsMenu from '../TabsMenu';
 import ThemeTab from './ThemeTab';
@@ -10,13 +10,49 @@ import styled from 'styled-components';
 
 const tabs = [
   { id: 1, title: 'Themes', content: ThemeTab },
-  { id: 2, title: 'Desktop', content: DesktopTab, defaultTab: true },
+  { id: 2, title: 'Desktop', content: DesktopTab },
   { id: 3, title: 'Screen Saver', content: ScreenSaverTab },
   { id: 4, title: 'Appearance', content: AppearanceTab },
   { id: 5, title: 'Settings', content: SettingsTab },
 ];
 
-function DisplayProperties() {
+const initialState = {
+  desktop: {
+    type: 'url', //'url' || 'color'
+    background: '../../../assets/properties/displayProperties/preview.png', //'url string' / color (rgb || hex)
+  },
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DESKTOP':
+      return {
+        ...state.desktop,
+        type: action.url,
+        background: action.background,
+      };
+    default:
+      break;
+  }
+};
+
+function DisplayProperties({ onClose }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleCancel = () => {
+    console.log('close without applying changes');
+    onClose();
+  };
+
+  const handleApply = () => {
+    console.log('apply changes');
+  };
+
+  const handleOk = () => {
+    console.log('make changes and close');
+    onClose();
+  };
+
   return (
     <Properties>
       <TabsMenu tabs={tabs} />
