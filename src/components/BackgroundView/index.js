e0eb2a295c222e79a455137d322618a2b724be35
Tomer Matmon <tomer.matmon@reallygood.co.il>
2021-04-13 12:32:17 +0300

    Fix background preview

--- src/components/BackgroundView/index.js
+++ src/components/BackgroundView/index.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
+
+const getImageWidth = src => {
+  const shadowImage = new Image();
+  shadowImage.src = src;
+  return shadowImage.width;
+};
 
 function BackgroundView({ background }) {
   const { position, image, color } = background;
-  return <Background position={position} image={image} color={color} />;
+  const [width, setWidth] = useState();
+  const backgroundRef = useRef(null);
+
+  const imagewidth = getImageWidth(image);
+
+  useEffect(() => {
+    if (!backgroundRef.current) return;
+
+    const backgroundWidth = backgroundRef.current.clientWidth;
+    const windowWidth = window.innerWidth;
+
+    if (backgroundWidth !== windowWidth && imagewidth > 0) {
+      setWidth((imagewidth / windowWidth) * 100);
+    }
+  }, [image, imagewidth, position]);
+
+  return (
+    <Background
+      ref={backgroundRef}
+      position={position}
+      image={image}
+      color={color}
+      size={width}
+    />
+  );
 }
 
 const Background = styled.div`
   width: 100%;
   height: 100%;
   pointer-events: none;
-  ${({ image, color, position }) => {
-    return `background-image: url(${image});
+  ${({ image, color, position, size }) => {
+    return `
+      background-image: url(${image});
       background-color: ${color}
       background-repeat: ${position === 'tile' ? 'repeat' : 'no-repeat'};
       background-position: ${position === 'tile' ? 'top left' : 'center'};
-      background-size: ${position === 'stretch' ? '100% 100%' : ''}; 
+      background-size: ${position === 'stretch' ? '100% 100%' : `${size}%`}; 
     `;
   }}
 `;
