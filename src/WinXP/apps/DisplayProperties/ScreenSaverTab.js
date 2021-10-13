92764d5a54758e7319accc788e5accba542baa3e
Ori Shema <ori@reallygood.co.il>
2021-10-13 17:35:41 +0300

    Add preview area

--- src/WinXP/apps/DisplayProperties/ScreenSaverTab.js
+++ src/WinXP/apps/DisplayProperties/ScreenSaverTab.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import display from '../../../assets/properties/displayProperties/display.png';
 
 function ScreenSaverTab() {
-  return <div>screen saver tab</div>;
+  return (
+    <ScreenSaver>
+      <div className="preview">
+        <img src={display} alt="display" />
+        <div className="display-overlay"></div>
+      </div>
+    </ScreenSaver>
+  );
 }
+
+const ScreenSaver = styled.div`
+  .preview {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    margin: 10px 0;
+
+    & .display-overlay {
+      position: absolute;
+      top: 17px;
+      left: 88px;
+      width: 170px;
+      height: 118px;
+      background-color: #2f71cd;
+    }
+  }
+`;
 
 export default ScreenSaverTab;
