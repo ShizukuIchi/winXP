b6a8ce58dca7cb5bd0a5ec65bf488eadf0d431d8
Ori Shema <ori@reallygood.co.il>
2021-10-20 11:06:40 +0300

    Add number input labeling

--- src/WinXP/apps/DisplayProperties/ScreenSaverTab.js
+++ src/WinXP/apps/DisplayProperties/ScreenSaverTab.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import display from '../../../assets/properties/displayProperties/display.png';
 
 import arrowDown from 'assets/properties/displayProperties/icons/arrowDown.png';
+import Button from 'components/Button';
+import NumberInput from 'components/numberInput';
 
 function ScreenSaverTab() {
   return (
@@ -23,6 +25,15 @@ function ScreenSaverTab() {
               <option value="Windows XP">Windows XP</option>
             </select>
           </label>
+          <div className="button-group">
+            <Button style={{ marginLeft: '7px' }}>Settings</Button>
+            <Button style={{ marginLeft: '9px' }}>Preview</Button>
+          </div>
+          <div className="quickSettings">
+            <label className="label">Wait:</label>
+            <NumberInput defaultValue={5} />
+            <p>minutes</p>
+          </div>
         </fieldset>
         <fieldset className="settings">
           <legend>Monitor power</legend>
@@ -50,47 +61,67 @@ const ScreenSaver = styled.div`
   }
   .config-area {
     margin-top: 4.5px;
+  }
 
-    .settings {
-      position: relative;
-      border: 1px solid #e1e1d4;
-      border-radius: 5px;
-      font-size: 11px;
-      height: 80px;
+  .settings {
+    position: relative;
+    border: 1px solid #e1e1d4;
+    border-radius: 5px;
+    font-size: 11px;
+    height: 80px;
+  }
+
+  .settings:nth-child(2) {
+    height: 87.5px;
+    margin-top: 4px;
+  }
+
+  legend {
+    margin-left: 9px;
+  }
+
+  .position-input {
+    border-radius: 0;
+    border-color: grey;
+    font-size: 12px;
+    margin-left: 9px;
+    margin-top: 6.5px;
+    width: 167px;
+    height: 22px;
+    &:focus {
+      color: #fff;
+      background-color: #2f71cd;
+      box-shadow: inset 0px 0px 0px 2px #fff;
+      outline: none;
     }
+  }
 
-    .settings:nth-child(2) {
-      height: 87.5px;
+  .arrow-down {
+    position: absolute;
+    left: 159px;
+    top: 7px;
+    width: 16px;
+    height: 20px;
+    pointer-events: none;
+  }
+
+  .button-group {
+    display: inline-block;
+    position: absolute;
+    top: 5px;
+  }
+
+  .quickSettings {
+    display: flex;
+    margin-top: 8px;
+    & label {
+      margin-top: 3.5px;
+      margin-left: 8px;
+      padding-right: 2.5px;
+    }
+    & p {
+      margin-left: 5px;
       margin-top: 4px;
-    }
-
-    legend {
-      margin-left: 9px;
-    }
-
-    .position-input {
-      border-radius: 0;
-      border-color: grey;
-      font-size: 12px;
-      margin-left: 9px;
-      margin-top: 5.5px;
-      width: 167px;
-      height: 22px;
-      &:focus {
-        color: #fff;
-        background-color: #2f71cd;
-        box-shadow: inset 0px 0px 0px 2px #fff;
-        outline: none;
-      }
-    }
-
-    .arrow-down {
-      position: absolute;
-      left: 159px;
-      top: 6px;
-      width: 16px;
-      height: 20px;
-      pointer-events: none;
     }
   }
 `;
