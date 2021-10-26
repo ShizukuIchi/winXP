a7d2b905e04e75c747ee51386cd3c75479e9ef3b
Ori Shema <ori@reallygood.co.il>
2021-10-26 17:07:30 +0300

    Create Blank screen saver

---
+++ src/components/ScreenSavers/Blank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Blank() {
+  return <StyledWindowsXP></StyledWindowsXP>;
+}
+
+const StyledWindowsXP = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+`;
+
+export default Blank;
