ec434fd5a62acd6b2cbc79965dbfb8453505ab41
Dylan Smith <dylanatsmith@github.com>
2023-10-06 15:55:55 +0100

    Fix Minesweeper score and number wrapping

--- src/WinXP/apps/Minesweeper/MinesweeperView.js
+++ src/WinXP/apps/Minesweeper/MinesweeperView.js
@@ -367,6 +367,7 @@ export default styled(MineSweeperView)`
     border-width: 0 1px 1px 0;
     border-style: solid;
     border-color: #fff;
+    display: flex;
     text-align: right;
   }
   .mine__face__outer {
